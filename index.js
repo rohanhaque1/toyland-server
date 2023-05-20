3e8462272168cfe1ca62f4ec7336856e3563186a
rohan-haque <rohanhaque234@gmail.com>
2023-05-20 12:13:21 +0600

    delete method added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express()
@@ -41,7 +41,12 @@ async function run() {
           res.send(result)
       })
 
-
+    app.delete('toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.deleteOne(query)
+      res.send(result)
+      })
 
 
 
